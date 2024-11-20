e6905f96a8ddbb35ee940429740d70eb36af806b
Matvey Smorodin <mtv.smorodin@gmail.com>
2024-11-20 18:53:47 +0300

    init commit

---
+++ sketchybar/colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export WHITE=0xfffffff
+export BLACK=0x00000000
+
+# -- Teal scheme --
+export BAR_COLOR=$BLACK
+export ITEM_BG_COLOR=$WHITE #0xff003537
+export ACCENT_COLOR=$WHITE #0xff2cf9ed
